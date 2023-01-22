b361d9f0e4280bd9606bf234da77e1b2f3ba9015
Prashant-1695 <prashant33968@gmail.com>
2023-01-22 23:38:16 +0530

    build | V

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 # Normal build steps
 . build/envsetup.sh
-lunch p404_lavender-userdebug
+lunch lineage_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 
 compile_plox () {
 make bacon -j10
