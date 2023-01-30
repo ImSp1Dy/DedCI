67eed7300657e5ed910a622a6f628308b19ee20d
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-01-30 18:35:46 +0530

    Vanilla | R

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ lunch spark_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
+# export WITH_GAPPS=true
 
 compile_plox () {
 mka bacon -j10
